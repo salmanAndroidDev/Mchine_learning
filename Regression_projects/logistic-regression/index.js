e52c4af37a15304fd5d7c64cacd1cae9a6603384
Salman Barani <salmanAndroidDev@yahoo.com>
2020-05-08 01:03:50 +0430

    finish logistic regression algorithm with single label

--- Regression_projects/logistic-regression/index.js
+++ Regression_projects/logistic-regression/index.js
@@ -1,7 +1,8 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
-const loadCsv = require('../load-csv')
-
+const loadCsv = require('../load-csv');
+const LogisticRegression = require('./logistic-regression');
+const plot = require('node-remote-plot');
 
 const {features, labels, testFeatures, testLabels} = loadCsv('../data/cars.csv', {
     dataColumns:['horsepower', 'displacement', 'weight'],
@@ -14,5 +15,18 @@ const {features, labels, testFeatures, testLabels} = loadCsv('../data/cars.csv',
 })
 
 
-console.log(testLabels);
+const regression = new LogisticRegression(features, labels, {
+    learningRate: 0.5,
+    iterations: 10,
+    batchSize: 10,
+    decisionBoundary: 0.1
+});
 
+
+regression.train();
+console.log(regression.test(testFeatures, testLabels));
+
+
+plot({
+    x: regression.costHistory.reverse()
+})
