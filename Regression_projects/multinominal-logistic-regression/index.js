6afd668c996ef8a651d02b6c2cdc6cf6868d51b0
Salman Barani <salmanAndroidDev@yahoo.com>
2020-05-09 17:34:55 +0430

    create and set features and labels for image classification

--- Regression_projects/multinominal-logistic-regression/index.js
+++ Regression_projects/multinominal-logistic-regression/index.js
@@ -1,40 +1,17 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
-const loadCsv = require('../load-csv');
 const LogisticRegression = require('./logistic-regression');
 const plot = require('node-remote-plot');
 const _ = require('lodash');
+const mnist = require('mnist-data');
 
+const mnistData = mnist.training(0,10);
 
-const {features, labels, testFeatures, testLabels} = loadCsv('../data/cars.csv', {
-    dataColumns:['horsepower', 'displacement', 'weight'],
-    labelColumns:['mpg'],
-    shuffle: true,
-    splitTest: 50,
-    converters: {
-        mpg: value => {
-            const mpg = parseFloat(value);
-
-            if(mpg < 15)
-                return [1,0,0];
-            else if (mpg < 30 )
-                return [0,1,0]
-            else
-                return [0,0,1]
-        }
-    }    
+const features = mnistData.images.values.map(image => _.flatMap(image));
+const labels = mnist.training(0,10).labels.values.map(label => {
+    const row = new Array(10).fill(0);  // this will create array, size of 10;
+    row[label] = 1;
+    return row;
 });
 
-const regression = new LogisticRegression(features, _.flatMap(labels), {
-    learningRate: 0.5,
-    iterations: 100,
-    batchSize: 10,
-});
-
-
-regression.train();
-console.log(regression.test(testFeatures, _.flatMap(testLabels) ));
-// regression.predict([[150, 200, 2.223]]).print();
-// plot({
-//     x: regression.costHistory.reverse()
-// })
+console.log(labels);
