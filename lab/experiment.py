9a6a2be27488152f1609158065a8c6576ae89735
Salman Barani <salmanAndroidDev@yahoo.com>
2020-08-01 10:23:58 +0430

    create experimental neuron for deeper understanding

--- lab/experiment.py
+++ lab/experiment.py
@@ -5,16 +5,23 @@ def sigmoid(x):
     return 1/ (1 + np.exp(-x))
 
 raw_data = [
-    'terrible',
-    'bad thing',
-    'good',
-    'not bad',
-    'not good',
-    'amazing',
+    'so terrible dont buy',
+    'terrible and bad and bullshit',
+    'bad idea behind this game',
+    'good idea behind this game',
+    'not bad actually it is good and amazing',
+    'not good actually it is bad and terrible and bullshit',
+    'amazing idea with the best',
     'bullshit and terible and bad',
+    'it is super good and the best',
+    'what the fuck with this bad and bullshit and terrible idea',
+    'it got a git golden buzzar for this nice and good and amazing idea',
+    'not that much bad, actually it is good',
+    'not good at all, actually it is bad never buy it',
+    'amazing idea it worth buying its so good so clever so amazing'
 ]
 
-label_data = np.array([0,0,1,1,0,1,0])
+label_data = np.array([0,0,0,1,1,0,1,0,0,0,1,1,0,1])
 
 token = list(map(lambda sentence: set(sentence.split(' ')) ,raw_data))
 
@@ -32,6 +39,7 @@ for index, word in enumerate(vocabs):
     word2index[word]= index
 
 input_dataset = list()
+
 for sent in token:
     raw_sent = list()
     for word in sent:
@@ -41,9 +49,8 @@ for sent in token:
             ""    
     input_dataset.append(list(set(raw_sent)))
 
-
 alpha, iteration = (0.1, 300)
-hidden_size = 5
+hidden_size = 3
 
 weights_0_1 = 0.2*np.random.random((len(vocabs),hidden_size)) - 0.1
 weights_1_2 = 0.2*np.random.random((hidden_size,1)) - 0.1
@@ -69,10 +76,30 @@ for iter in range(iteration):
         if (np.abs(layer_2_delta) < 0.5):
             correct += 1
 
-        if iter == 110:
-            print(layer_2 , ' == ', y)
-
     if correct > total:
         total = correct
         best_rond = iter
-    print("Best iteration is: ", best_rond,' Acuraccy: ', (total/ len(label_data)) * 100,'%')         
+    print("Best iteration is: ", best_rond,' Acuraccy: ', (total/ len(label_data)) * 100,'%', ' current ', iter)
+    if iter >= 66: break
+
+while(True):
+    sentence = input("Enter the sentence: ")
+
+    if sentence  == 'q':
+        break
+    else:
+        sent = sentence.split(' ')
+        raw_sentence = list()
+        for word in sent:
+            try:
+                raw_sentence.append(word2index[word])
+            except e:
+                ""   
+        x = list(set(raw_sentence))
+
+        layer_1 = sigmoid(np.sum(weights_0_1[x], axis= 0))
+        layer_2 = sigmoid(np.dot(layer_1, weights_1_2))
+            
+        message = "prediction:{} weight: {}".format(layer_2, weights_0_1[x])
+        
+        print(message)
