a484d5c400f04737361da34b64a8c1f663cd38db
Salman Barani <salmanAndroidDev@yahoo.com>
2020-08-10 13:26:48 +0430

    cross entropy for multi-classes

--- lab/experiment.py
+++ lab/experiment.py
@@ -1,82 +1,23 @@
 import numpy as np
-# Setting the random seed, feel free to change it and see different solutions.
-np.random.seed(42)
+# 0 = salman
+# 1 = ali
+# 2 = vahab
 
-w = np.random.random((2))
-b = 1
-lr = 0.1
+P = np.array([ # all the probabilities that we belive have happened.
+    [0.2, 0.3, 0.1]
+])
 
-def stepFunction(t):
-    if t >= 0:
-        return 1
-    return 0
-
-def prediction(X, W, b):
-    return stepFunction((np.matmul(X,W)+b))
-
-def test(x,y):
+def cross_entropy():
+    result = []
+    Y = np.array([
+        [1,0,1],
+        [0,1,0],
+    ])
     sum = 0
-    for i in range(len(y)):
-        pred = prediction(x[i],w,b)
-        
-        if pred == y[i]:
-            sum += 1
-    return (sum / len(y)) * 100        
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            sum += Y[i][j] * -np.log(P[i][j])
+    print(sum)
 
-# TODO: Fill in the code below to implement the perceptron trick.
-# The function should receive as inputs the data X, the labels y,
-# the weights W (as an array), and the bias b,
-# update the weights and bias W, b, according to the perceptron algorithm,
-# and return W and b.
-def perceptronStep(X, y, W, b, learn_rate = 0.01):
-    # Fill in code
-    
-    for i in range(len(X)):
-        y_hat = prediction(X[i],W,b)
-                
-        if y_hat < y[i]:
-            W += X[i] * learn_rate
-            b += learn_rate
-        elif y_hat > y[i]:
-            W -= X[i] * learn_rate
-            b -= learn_rate
-            
-# This function runs the perceptron algorithm repeatedly on the dataset,
-# and returns a few of the boundary lines obtained in the iterations,
-# for plotting purposes.
-# Feel free to play with the learning rate and the num_epochs,
-# and see your results plotted below.
-def trainPerceptronAlgorithm(X, y, learn_rate = 0.01, num_epochs = 25):
-    x_min, x_max = min(X.T[0]), max(X.T[0])
-    y_min, y_max = min(X.T[1]), max(X.T[1])
-    W = np.array(np.random.rand(2,1))
-    b = np.random.rand(1)[0] + x_max
-    # These are the solution lines that get plotted below.
-    boundary_lines = []
-    for i in range(num_epochs):
-        # In each epoch, we apply the perceptron step.
-        W, b = perceptronStep(X, y, W, b, learn_rate)
-        boundary_lines.append((-W[0]/W[1], -b/W[1]))
-    return boundary_lines
-
-#// here we start testing
-file = open('data.csv','r')
-raw_data = file.readlines()
-file.close()
-
-input = []
-label = []
-
-for data in raw_data:
-    x1,x2,y = data.split(',')
-    input.append([float(x1),float(x2)])
-    label.append(int(y))
-
-
-input = np.array(input)
-
-for i in range(10):
-    perceptronStep(input,label,w,b,lr)
-    print(test(input,label))
-
-# print(trainPerceptronAlgorithm(input,y))
+result = cross_entropy()
+print(result)
