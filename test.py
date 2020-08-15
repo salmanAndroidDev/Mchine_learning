75f184b5cac12073de18bcbf3daff8302c3ab560
Salman Barani <salmanAndroidDev@yahoo.com>
2020-08-16 01:37:27 +0430

    add 3 layer neural network

--- test.py
+++ test.py
@@ -1,39 +1,45 @@
 import numpy as np
-# Setting the random seed, feel free to change it and see different solutions.
-np.random.seed(42)
 
-def stepFunction(t):
-    if t >= 0:
-        return 1
-    return 0
+def sigmoid(x):
+    """
+    Calculate sigmoid
+    """
+    return 1 / (1 + np.exp(-x))
 
-def prediction(X, W, b):
-    return stepFunction((np.matmul(X,W)+b)[0])
 
-# TODO: Fill in the code below to implement the perceptron trick.
-# The function should receive as inputs the data X, the labels y,
-# the weights W (as an array), and the bias b,
-# update the weights and bias W, b, according to the perceptron algorithm,
-# and return W and b.
-def perceptronStep(X, y, W, b, learn_rate = 0.01):
-    # Fill in code
-    return W, b
-    
-# This function runs the perceptron algorithm repeatedly on the dataset,
-# and returns a few of the boundary lines obtained in the iterations,
-# for plotting purposes.
-# Feel free to play with the learning rate and the num_epochs,
-# and see your results plotted below.
-def trainPerceptronAlgorithm(X, y, learn_rate = 0.01, num_epochs = 25):
-    x_min, x_max = min(X.T[0]), max(X.T[0])
-    y_min, y_max = min(X.T[1]), max(X.T[1])
-    W = np.array(np.random.rand(2,1))
-    b = np.random.rand(1)[0] + x_max
-    # These are the solution lines that get plotted below.
-    boundary_lines = []
-    for i in range(num_epochs):
-        # In each epoch, we apply the perceptron step.
-        W, b = perceptronStep(X, y, W, b, learn_rate)
-        boundary_lines.append((-W[0]/W[1], -b/W[1]))
-    return boundary_lines
+x = np.array([0.5, 0.1, -0.2])
+target = 0.6
+learnrate = 0.5
 
+weights_input_hidden = np.array([[0.5, -0.6],
+                                 [0.1, -0.2],
+                                 [0.1, 0.7]])
+
+weights_hidden_output = np.array([0.1, -0.3])
+
+## Forward pass
+hidden_layer_input = np.dot(x, weights_input_hidden)
+hidden_layer_output = sigmoid(hidden_layer_input)
+
+output_layer_in = np.dot(hidden_layer_output, weights_hidden_output)
+output = sigmoid(output_layer_in)
+## Backwards pass
+## TODO: Calculate output error
+error = target - output
+
+# TODO: Calculate error term for output layer
+output_error_term = error * output * (1 - output)
+
+# TODO: Calculate error term for hidden layer
+hidden_error_term = np.dot(output_error_term, weights_hidden_output) * (hidden_layer_output * (1 - hidden_layer_output))
+
+# TODO: Calculate change in weights for hidden layer to output layer
+delta_w_h_o = learnrate * output_error_term * hidden_layer_output
+
+# TODO: Calculate change in weights for input layer to hidden layer
+delta_w_i_h = learnrate * hidden_error_term * x 
+
+print('Change in weights for hidden layer to output layer:')
+print(delta_w_h_o)
+print('Change in weights for input layer to hidden layer:')
+print(delta_w_i_h)
